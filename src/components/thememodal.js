6d7f9ae599c3cc012d2a3a83a2e72c4408722fc7
Abdulai Musah <abdulaimusah31@gmail.com>
2023-05-13 09:22:51 +0000

    modified close button for theme modal

--- src/components/thememodal.js
+++ src/components/thememodal.js
@@ -14,10 +14,10 @@ const ThemeModal = ({ setThemeButton, setThemeColor }) => {
 				<div className="flex justify-end">
 					<button
 						className="justicfy-end inline-block
-                bg-white mb-4 bg-transparent text-white
+                b mb-4 text-white
 				font-extrabold text-2xl"
 						onClick={() => setThemeButton(false)}
-					> X {/*
+					>  
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="20"
@@ -28,7 +28,7 @@ const ThemeModal = ({ setThemeButton, setThemeColor }) => {
 						>
 							<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
 							<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-						</svg> */}
+						</svg> 
 					</button>
 				</div>
 
